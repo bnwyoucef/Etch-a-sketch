81c49c8305ef45fbe20662d32b98f37cc7039b51
bnwyoucef <y.bounoua@esi-sba.dz>
2022-01-07 16:48:21 +0100

    clear canvas

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ const ctx = myCanvas.getContext('2d');
 ctx.lineWidth = 10;
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+const CHANGE_VALUE = 10;
+let hue = 142;
+ctx.strokeStyle = `hsl(${hue}, 100%,50%)`;
 ctx.beginPath(); // to start drawing
 const { width, height } = myCanvas; //get the height and width of the canvas
-const x = Math.floor(Math.random() * width); //set the x value between 0 and width
-const y = Math.floor(Math.random() * height); //set the y value between 0 and height
+let x = Math.floor(Math.random() * width); //set the x value between 0 and width
+let y = Math.floor(Math.random() * height); //set the y value between 0 and height
 ctx.moveTo(x, y);
 ctx.lineTo(x, y);
 ctx.stroke();
@@ -18,6 +21,42 @@ window.addEventListener('keydown', handleArrows);
 function handleArrows(event) {
     if (event.key.includes('Arrow')) {
         event.preventDefault();
+        draw(event.key);
         console.log(event.key);
     }
 }
+//handle keys
+function draw(key) {
+    hue += 5;
+    ctx.strokeStyle = `hsl(${hue},100%,50%)`;
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    switch (key) {
+        case 'ArrowUp':
+            y -= CHANGE_VALUE;
+            break;
+        case 'ArrowDown':
+            y += CHANGE_VALUE;
+            break;
+        case 'ArrowLeft':
+            x -= CHANGE_VALUE;
+            break;
+        case 'ArrowRight':
+            x += CHANGE_VALUE;
+            break;
+    }
+    ctx.lineTo(x, y);
+    ctx.stroke();
+}
+
+//clear the canvas
+function clearCanvas() {
+    ctx.clearRect(0, 0, width, height);
+    myCanvas.classList.add('shake');
+    myCanvas.addEventListener('animationend', function() {
+        myCanvas.classList.remove('shake');
+    }, { once: true });
+}
+
+const clearBtn = document.querySelector('.shake');
+clearBtn.addEventListener('click', clearCanvas);
